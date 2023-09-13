72f18783c416a067468395a770553c919a3b21e5
keweiyao <wk42@phy.duke.edu>
2023-09-12 20:25:22 -0400

    first commit

---
+++ install_pdf.sh
@@ -0,0 +1,23 @@
+for it in nNNPDF30_nlo_as_0118_A108_Z54\
+ nNNPDF30_nlo_as_0118_A119_Z59\
+ nNNPDF30_nlo_as_0118_A12_Z6\
+ nNNPDF30_nlo_as_0118_A131_Z54\
+ nNNPDF30_nlo_as_0118_A14_Z7\
+ nNNPDF30_nlo_as_0118_A16_Z8\
+ nNNPDF30_nlo_as_0118_A184_Z74\
+ nNNPDF30_nlo_as_0118_A197_Z79\
+ nNNPDF30_nlo_as_0118_A208_Z82\
+ nNNPDF30_nlo_as_0118_A27_Z13\
+ nNNPDF30_nlo_as_0118_A2_Z1\
+ nNNPDF30_nlo_as_0118_A31_Z15\
+ nNNPDF30_nlo_as_0118_A40_Z20\
+ nNNPDF30_nlo_as_0118_A4_Z2\
+ nNNPDF30_nlo_as_0118_A56_Z26\
+ nNNPDF30_nlo_as_0118_A64_Z29\
+ nNNPDF30_nlo_as_0118_A6_Z3\
+ nNNPDF30_nlo_as_0118_A9_Z4\
+ nNNPDF30_nlo_as_0118_p;
+do 
+	lhapdf install $it
+done
+
